a3ccba6ed402c4a6fc3dd1df2e73c7aa4aaa1418
ViniciusSito <itoviniciusshigueyuki@gmail.com>
2026-03-10 19:31:30 -0300

    ajuste da torre de hanoi

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dados_avancados
 Matéria de Estruturas de Dados Avançadas
+
+- aula 10-03
+
+Problemas de recursividade:
+
+- escopo de variaveis;
+- depuração;
+- eficiência;
+ 
--- recursao/q1.c
+++ recursao/q1.c
@@ -2,23 +2,24 @@
 
 void torre(int n, char origem, char aux, char destino){
     //caso base
+    //no caso quando tem apenas um disco ele não utiliza o pino auxiliar apenas vai da origem para o destino
     if (n==1){
         printf("mova o disco 1 do pino %c para o pino %c\n", origem, destino);
         return;
     }
     //passo recursivo
     // vamos mover n-1 discos da origem para o auxiliar, usando o destino como apoio
-    torre(n-1, origem, aux, destino);
+    torre(n-1, origem, destino, aux);
 
     // vamos mover o disco maior (n) da origem para o destino
     printf("Mova o disco %d do pino %c para o pino %c\n", n, origem, destino);
 
     // por fim vamos mover os n-1 discos do auxiliar para o destino, usando a origem como apoio
-    torre(n-1, aux, destino, origem);
+    torre(n-1, aux, origem, destino);
 }
 // dentro da main é onde vai rodar rodar nossa torre, a gente define os pinos e o numero de discos que a gente quer resolver
 int main(){
-    int numerodiscos = 5;
+    int numerodiscos = 3;
     printf("Passo a passo para resolver a torre de Hanoi com %d discos: \n\n", numerodiscos);
 
     torre(numerodiscos, 'A', 'B', 'C');
